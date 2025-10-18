96596512d92dc7912c6a09831ec18caf6a7e9536
AnassEREKYSY <ereanass@gmail.com>
2025-10-18 15:39:22 +0200

    Corrected the E2E tests script -6

--- client/e2e/playwright.config.ts
+++ client/e2e/playwright.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
   testMatch: /.*\.spec\.ts/,
   testIgnore: ['**/src/**'],
-  timeout: 30 * 1000,
+  timeout: 30_000,
+  fullyParallel: true,
+  workers: 3,
+  forbidOnly: true,
+  retries: 1,
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: 'http://localhost:4200',
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
+    video: 'off',
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
   },
-  reporter: [['html', { open: 'never' }]],
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } }
+  ],
 });
